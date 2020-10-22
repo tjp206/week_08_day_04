4dbceefba1be81d61c68ad4446e7fd03cd988535
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-22 13:07:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <nav>
+
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="css" scoped>
+
+nav {
+  display: flex;
+  justify-content: center;
+  width: 50vw;
+  margin: 0 auto;
+}
+
+#view {
+  width: 50vw;
+  margin: 0 auto;
+}
+
+a{
+  margin: 0 20px;
+}
+
+</style>
